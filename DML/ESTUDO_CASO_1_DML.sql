e6a84b3bff4d56e06945d5e85ece90d57a1ccb4b
lucasmissagia <65687763+Tydriesh@users.noreply.github.com>
2024-11-09 23:48:29 -0300

    comiit atualização EC1 e 2

--- DML/ESTUDO_CASO_1_DML.sql
+++ DML/ESTUDO_CASO_1_DML.sql
@@ -7,12 +7,28 @@ insert into fornecedores values
 (4, "Loja do João", "Avenida Paulista", 1500, "Bela Vista", "São Paulo", "SP", "Brasil", "01310-200", "1199988-7766", "João Fernandes"),
 (5, "Padaria Pão Quente", "Rua das Flores", 35, "Jardim das Rosas", "Curitiba", "PR", "Brasil", "80520-100", "4198877-3344", "Ana Clara");
 
+update fornecedores
+set fornec_contato = "Pedro Antonio"
+where fornec_cod = 2;
+
+update fornecedores
+set fornec_rua = 'Rua dom Pedro'
+where fornec_cod = 3;
+
 insert into filiais values
 (1,"Filial São José","Rua Dr Antonio Basilio",31,"Jardim da Penha","Vitoria","ES","Brasil","29060-390",200),
 (2, "Filial Zona Sul", "Rua das Laranjeiras", 456, "Laranjeiras", "Rio de Janeiro", "RJ", "Brasil", "22240-003", 350),
 (3, "Filial Norte Shopping", "Avenida Dom Hélder Câmara", 5474, "Cachambi", "Rio de Janeiro", "RJ", "Brasil", "20771-004", 500),
 (4, "Filial Paulista", "Avenida Brigadeiro Faria Lima", 1010, "Pinheiros", "São Paulo", "SP", "Brasil", "01452-000", 600),
 (5, "Filial Centro Histórico", "Rua dos Andradas", 789, "Centro Histórico", "Porto Alegre", "RS", "Brasil", "90020-005", 450);
+
+update filiais
+set filial_capacidade = 1000
+where filial_cod = 5;
+
+update filiais
+set filial_numrua = 600
+where filial_cod = 3;
 
 insert into produtos values
 (1,"Celular X","Celular Azul","Produto de 128gb de memoria na cor azul",15,1200.00,"UN",5),
@@ -21,13 +37,28 @@ insert into produtos values
 (4, "Fone de Ouvido", "Fone Bluetooth", "Fone de ouvido Bluetooth com cancelamento de ruído", 50, 200.00, "UN", 10),
 (5, "Smartwatch S", "Relógio Inteligente Preto", "Smartwatch com monitoramento de frequência cardíaca", 20, 800.00, "UN", 4);
 
+UPDATE PRODUTOS 
+SET PROD_PRECOUNIT = 49.99 
+WHERE PROD_COD = 10;
+
+UPDATE PRODUTOS 
+SET PROD_QUANT = 150.000 
+WHERE PROD_COD = 5;
+
 insert into pedidos values
 (1, '2024-11-01', '09:30:00', '2024-11-10', 'PENDENTE',1),
 (2, '2024-11-02', '10:15:00', '2024-11-12', 'EM ESPERA',2),
 (3, '2024-11-03', '11:45:00', '2024-11-13', 'CONCLUÍDO',3),
 (4, '2024-11-04', '13:20:00', '2024-11-14', 'PENDENTE',4),
-(5, '2024-11-24', '15:20:10', '2024-11-14', 'CONCLUÍDO',4),
-(6, '2024-11-05', '15:00:00', '2024-11-15', 'PENDENTE',5);
+(5, '2024-11-24', '15:20:10', '2024-11-14', 'CONCLUÍDO',5);
+
+UPDATE PEDIDOS 
+SET PED_STATUS = 'CONCLUÍDO'
+WHERE PED_CODIGO = 1;
+
+UPDATE PEDIDOS 
+SET PED_PREVISAO = '2024-11-20' 
+WHERE PED_CODIGO = 2;
 
 insert into recebimentos values
 ('2024-11-01', '08:30:00', 150, 'Aprovado', 1),
@@ -36,24 +67,75 @@ insert into recebimentos values
 ('2024-11-04', '11:00:00', 275, 'Aprovado', 4),
 ('2024-11-05', '14:30:00', 120, 'Em espera', 5);
 
+UPDATE RECEBIMENTOS 
+SET RECEB_QUANT_PROD = 200.000
+WHERE RECEB_PEDIDOS = 50;
+
+UPDATE RECEBIMENTOS 
+SET RECEB_CONDICAO = 'Recebido em bom estado'
+WHERE RECEB_PEDIDOS = 50;
+
 insert into pedidos_produtos values
-(1, 2, 15),
-(1, 3, 20),
-(2, 5, 50),
+(1, 1, 15),
+(1, 2, 20),
+(2, 3, 50),
 (3, 4, 10),
-(4, 1, 25);
+(4, 5, 25);
+
+UPDATE PEDIDOS_PRODUTOS 
+SET PDPR_QUANT = 5.000
+WHERE PDPR_PEDIDOS = 1001 AND PDPR_PRODUTOS = 5;
+
+UPDATE PEDIDOS_PRODUTOS 
+SET PDPR_QUANT = 100
+WHERE PDPR_PEDIDOS = 1;
 
 insert into filiais_produtos values
 (1, 1, 100),
 (1, 2, 200),
 (2, 3, 150),
-(3, 4, 300),
-(4, 5, 50);
+(3, 3, 300),
+(4, 4, 50);
+
+UPDATE FILIAIS_PRODUTOS
+SET FLPR_QUANT = 500
+WHERE FLPR_FILIAL = 1;
 
 insert into fornecedores_produtos values
-(1, 5),
-(2, 4),
-(3, 3),
-(4, 2),
-(5, 1),
-(1, 2);
+(1, 1,5),
+(2, 2,10),
+(3, 3,15),
+(4, 4,20),
+(5, 5,25);
+
+UPDATE FORNECEDORES_PRODUTOS
+SET FRPR_QTD = 50
+WHERE FRPR_FORNECEDOR = 1;
+
+UPDATE FORNECEDORES_PRODUTOS
+SET FRPR_QTD = 55
+WHERE FRPR_FORNECEDOR = 2;
+
+DELETE FROM FORNECEDORES_PRODUTOS
+WHERE FRPR_FORNECEDOR = 5;
+
+DELETE FROM FILIAIS_PRODUTOS
+WHERE FLPR_FILIAL = 5;
+
+DELETE FROM PEDIDOS_PRODUTOS
+WHERE PDPR_PRODUTOS = 5;
+
+DELETE FROM RECEBIMENTOS
+WHERE RECEB_PEDIDOS = 5;
+
+DELETE FROM PEDIDOS
+WHERE PED_CODIGO = 5;
+
+DELETE FROM PRODUTOS
+WHERE PROD_COD = 5;
+
+DELETE FROM FILIAIS
+WHERE FILIAL_COD = 5;
+
+DELETE FROM FORNECEDORES
+WHERE FORNEC_COD = 5;
--- DML/ESTUDO_CASO_2_DML.sql
+++ DML/ESTUDO_CASO_2_DML.sql
@@ -7,12 +7,28 @@ insert into passageiros values
 (4, '321.654.987-00', 'Daniel Costa', '(41)99876-5432', 'Av. Paraná, 321, Curitiba, PR', 'daniel.costa@email.com'),
 (5, '654.987.321-00', 'Elaine Souza', '(51)91234-5678', 'Rua do Porto, 654, Porto Alegre, RS', 'elaine.souza@email.com');
 
+update passageiros
+set nome_pass = 'Alice Pedrosa'
+where id_pass = 1;
+
+update passageiros
+set tel_pass = '(27)99999-8888'
+where id_pass = 3;
+
 insert into aeroportos values
 (1, 'Aeroporto Internacional de Guarulhos', 'São Paulo', 'Brasil', -46.469511, -23.435556),
 (2, 'Aeroporto Internacional de Los Angeles', 'Los Angeles', 'Estados Unidos', -118.408075, 33.941589),
 (3, 'Aeroporto de Heathrow', 'Londres', 'Reino Unido', -0.454295, 51.470020),
 (4, 'Aeroporto de Changi', 'Cingapura', 'Cingapura', 103.987739, 1.364420),
 (5, 'Aeroporto Internacional de Tóquio', 'Tóquio', 'Japão', 139.779839, 35.549393);
+
+update aeroportos
+set nome_aeroporto = 'Aeroporto Internacional de LA'
+where id_aeroporto = 2;
+
+update aeroportos
+set nome_aeroporto = 'Aeroporto de Londres'
+where id_aeroporto = 3;
 
 insert into funcionarios values
 (1, 'Carlos Pereira', 'Gerente', '1985-03-15', '(11)98765-4321'),
@@ -21,6 +37,14 @@ insert into funcionarios values
 (4, 'Fernanda Silva', 'Coordenadora', '1992-12-05', '(41)97654-3210'),
 (5, 'Bruno Santos', 'Assistente', '1995-08-10', '(51)93456-7890');
 
+update funcionarios
+set funcao_func = 'Diretora Analista'
+where nome_func = 'Ana Souza';
+
+update funcionarios
+set nome_func = 'Bruna Silva'
+where funcao_func ='Assistente';
+
 insert into aeronaves values
 (1, 'Boeing 737-800', '2015-06-15', 189, 'Boeing'),
 (2, 'Airbus A320', '2018-04-10', 180, 'Airbus'),
@@ -28,12 +52,28 @@ insert into aeronaves values
 (4, 'Boeing 787-9', '2017-02-28', 296, 'Boeing'),
 (5, 'Airbus A330-200', '2014-11-05', 246, 'Airbus');
 
+update aeronaves
+set fabricante_aeronave = 'Boeing'
+where prefixo_aeronave = 5;
+
+update aeronaves
+set capacidade_aeronave = 320
+where prefixo_aeronave = 3;
+
 insert into voos values
 (1, '2024-11-15 08:00:00', '2024-11-15 12:00:00', 1, 2, 1),
-(2, '2024-11-16 09:30:00', '2024-11-16 13:30:00', 3, 4, 2),
-(3, '2024-11-17 07:15:00', '2024-11-17 11:15:00', 2, 5, 3),
-(4, '2024-11-18 14:00:00', '2024-11-18 18:00:00', 5, 1, 4),
-(5, '2024-11-19 16:45:00', '2024-11-19 20:45:00', 4, 3, 5);
+(2, '2024-11-16 09:30:00', '2024-11-16 13:30:00', 2, 1, 2),
+(3, '2024-11-17 07:15:00', '2024-11-17 11:15:00', 3, 4, 3),
+(4, '2024-11-18 14:00:00', '2024-11-18 18:00:00', 4, 3, 4),
+(5, '2024-11-19 16:45:00', '2024-11-19 20:45:00', 5, 5, 5);
+
+update voos
+set id_aeronave = 3
+where id_voo = 2;
+
+update voos
+set aeroporto_origem = 4
+where id_voo = 2;
 
 insert into reservas values
 (1, 1),
@@ -42,9 +82,46 @@ insert into reservas values
 (4, 4),
 (5, 5);
 
+update reservas
+set id_pass = 2
+where id_voo = 1;
+
+update reservas
+set id_voo = 3
+where id_pass = 4;
+
 insert into equipes values
 (1, 1),
 (2, 2),
 (3, 3),
 (4, 4),
 (5, 5);
+
+update equipes
+set id_func = 2
+where id_voo = 3;
+
+update equipes
+set id_voo = 4
+where id_func = 1;
+
+delete from equipes
+where id_func = 5;
+
+delete from reservas
+where id_voo = 5;
+
+delete from voos
+where id_voo = 5;
+
+delete from aeronaves
+where prefixo_aeronave = 5;
+
+delete from funcionarios
+where id_func = 5;
+
+delete from aeroportos
+where id_aeroporto = 5;
+
+delete from passageiros
+where id_pass = 5;
